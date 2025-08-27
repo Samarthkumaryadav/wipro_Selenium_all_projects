4daef76873388b20b5fa54378911c8c9acbf1279
Samarth yadav <123004545+Samarthkumaryadav@users.noreply.github.com>
2025-08-28 03:59:34 +0530

    Update account.java

--- sam_cucumberBDD/src/test/java/account.java
+++ sam_cucumberBDD/src/test/java/account.java
@@ -10,7 +10,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class AccountActivitySteps {
+public class account {
 	WebDriver driver;
 	@Given("user logged in")
 	public void user_logged_in() {
